eea1575a369971c99a37641c7e8cbeb0a2f0128c
frog <95737177+frogchocolatebarpie@users.noreply.github.com>
2022-05-26 20:33:50 +0700

    chore: vite env

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-  baseURL: 'http://localhost:4001',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export function requestToken({ account, chainName, tokenName, signature }) {
